314af6306263fa7dd6c1010b442db709139ede5c
Fabio Amaral <fabiom2@gmail.com>
2022-09-10 16:48:21 -0300

    Add files

--- src/components/Card/CardAnimation/index.tsx
+++ src/components/Card/CardAnimation/index.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
-import { useWindowDimensions, ViewProps } from 'react-native';
+import React, { useEffect } from "react";
+import { useWindowDimensions, ViewProps } from "react-native";
 import {
   useAnimatedStyle,
   useSharedValue,
-  withTiming
-} from 'react-native-reanimated';
+  withTiming,
+} from "react-native-reanimated";
 
-import { AnimationContainer } from './styles';
+import { AnimationContainer } from "./styles";
 
 interface CardAnimationProps extends ViewProps {
   children: React.ReactNode;
@@ -16,23 +16,28 @@ export function CardAnimation({ children, ...rest }: CardAnimationProps) {
   const { width: displayWidth } = useWindowDimensions();
   const cardOpacity = useSharedValue(0);
   const cardOffset = useSharedValue(0.25 * displayWidth);
-
+  const DURATION_ONE_THOUSAND_MS = 1000;
   const animatedStyle = useAnimatedStyle(() => {
     return {
-      // TODO - setup animated style
-    }
-  })
+      opacity: cardOpacity.value,
+      transform: [{ translateX: cardOffset.value }],
+    };
+  });
 
   useEffect(() => {
-    /**
-     * TODO - setup cardOpacity.value and cardOffset.value with
-     * withTiming()
-     */
+    cardOpacity.value = withTiming(
+      1, {
+      duration: DURATION_ONE_THOUSAND_MS,
+    });
+    cardOffset.value = withTiming(
+      0, {
+      duration: DURATION_ONE_THOUSAND_MS,
+    });
   }, []);
 
   return (
     <AnimationContainer {...rest} style={animatedStyle}>
       {children}
     </AnimationContainer>
-  )
+  );
 }
